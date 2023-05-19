cd62105e45eef9b7581909d567157e5aeac474aa
JL Naypes <naypes.15@gmail.com>
2023-05-19 13:51:16 +0800

    New template with date now

--- src/time.js
+++ src/time.js
@@ -1,8 +1,10 @@
 const time = document.querySelector("#time");
 const day_now = document.querySelector("#day_now");
+const date_now = document.querySelector("#date_now");
 
 time.innerText = dayjs().format("h:mm A");
 day_now.innerText = dayjs().format("dddd");
+date_now.innerText = dayjs().format("MMM D, YYYY");
 
 // set delay
 setInterval(() => {
