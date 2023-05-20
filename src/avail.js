98f5197b392334a1840427680cbc68cfcdf42260
JL Naypes <naypes.15@gmail.com>
2023-05-20 15:58:38 +0800

    not avail = busy

--- src/avail.js
+++ src/avail.js
@@ -2,10 +2,10 @@ const days = document.querySelectorAll(".avail-btn");
 
 days.forEach((day) => {
   day.addEventListener("click", () => {
-    if (day.innerText === "not available") {
+    if (day.innerText === "busy") {
       day.innerText = "available";
     } else {
-      day.innerText = "not available";
+      day.innerText = "busy";
     }
   });
 });
